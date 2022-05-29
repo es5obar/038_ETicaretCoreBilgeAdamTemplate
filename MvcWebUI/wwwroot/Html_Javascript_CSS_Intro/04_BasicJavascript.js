cf8b91af7b3b3bb414abba70bead17ce91594f6b
Yahya Önder <06yahya06@gmail.com>
2022-05-29 23:47:59 +0300

    Add project files.

---
+++ MvcWebUI/wwwroot/Html_Javascript_CSS_Intro/04_BasicJavascript.js
@@ -0,0 +1,42 @@
+﻿function ucgenDoldur(tersten = false) {
+    var sonuc;
+    // var sonucArray = [];
+    var sonucArray = new Array();
+    for (var i = 0; i < 5; i++) {
+        sonuc = "";
+        for (var j = 0; j <= i; j++) {
+            sonuc += "*";
+        }
+        sonucArray[i] = sonuc;
+    }
+    sonuc = "";
+    if (!tersten) {
+        for (var i = 0; i < sonucArray.length; i++) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    }
+    else {
+        for (var i = sonucArray.length - 1; i >= 0; i--) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    }
+    //console.log(sonuc);
+    return sonuc;
+}
+function onLoadIleDoldur() {
+    var myTextArea = document.getElementById("mytextareaid");
+    myTextArea.value = ucgenDoldur();
+}
+
+var terstenGlobal = true;
+
+function onClickIleDoldur() {
+    var myTextArea = document.getElementById("mytextareaid");
+    myTextArea.value = ucgenDoldur(terstenGlobal);
+    var body = document.getElementsByTagName("body")[0];
+    if (terstenGlobal)
+        body.style.backgroundColor = "red";
+    else
+        body.style.backgroundColor = "yellow";
+    terstenGlobal = !terstenGlobal;
+}
---
+++ MvcWebUI/wwwroot/Html_Javascript_CSS_Intro/05_BasicJquery.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    console.log("script loaded");
+    var inputText;
+
+    $("#bSetText").click(function () {
+        inputText = $("#tbInput1").val(); // textbox , dropdownlist, listbox
+        $("#sOutput1").text(inputText);  // span, label, p
+    });
+
+    $("#bSetHtml").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#sOutput2").html('<label style="color:blue;font-size:24px;"><b>' + inputText + '</b></label>');
+    });
+
+    $("#bSetValue").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#tbOutput1").val(inputText);
+    });
+});
